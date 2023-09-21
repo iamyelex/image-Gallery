3d0e48845e72dba57ffe0058c85016afd6c1f059
iamyelex <yelex4love@gmail.com>
2023-09-21 15:20:41 -0700

    image gallery

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,31 @@
 import {
   Route,
   RouterProvider,
-  createBrowserRouter,
+  createHashRouter,
   createRoutesFromElements,
 } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import { loadImages, loadSearch } from "./services/APIs";
 import SearchResult from "./pages/SearchResult";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
-  const router = createBrowserRouter(
+  const router = createHashRouter(
     createRoutesFromElements(
       <Route>
-        <Route path="/" element={<Homepage />} loader={loadImages} />
+        <Route
+          path="/"
+          element={<Homepage />}
+          loader={loadImages}
+          errorElement={<ErrorPage message="something went wrong" />}
+        />
         <Route path="/login" element={<Login />} />
         <Route
           path="/search/:id"
           element={<SearchResult />}
           loader={loadSearch}
+          errorElement={<ErrorPage message="something went wrong" />}
         />
       </Route>
     )
